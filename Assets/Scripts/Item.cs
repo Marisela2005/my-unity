c4cfe2c9674ee9ac1a5ea2299ff3e19b2a384805
Marisela2005 <mariselavincenty@gmail.com>
2024-09-15 13:27:14 +0200

    Generando items

---
+++ Assets/Scripts/Item.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public int puntosGanados = 5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D collider)
+    {
+        NotificationCenter.DefaultCenter().PostNotification(this, "IncrementarPuntos", puntosGanados);
+        Destroy(gameObject);
+    }
+}
